1cd03925c5583a0b25c744d8bb03003ff773a06e
Ciaran Cushnahan <synonymsfornotverygood@gmail.com>
2022-06-21 11:16:14 +0100

    initial commit

---
+++ application/app.py
@@ -0,0 +1,28 @@
+'''
+
+Application to create report based on parameters passed
+
+'''
+
+
+# parse params
+
+# open/create new document
+
+# perform query
+
+# transform json response to list of object
+
+# work through list
+
+# pass data to template based on style parameters
+
+# process according to template
+
+# add to document
+
+# finish process
+
+# save document
+
+# end message
