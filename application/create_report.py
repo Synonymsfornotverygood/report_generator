81c1cfde693583632dd9ef44117fed1e36ce5daa
Ciaran Cushnahan <synonymsfornotverygood@gmail.com>
2022-07-05 13:14:11 +0100

    Update: Updated doc strings for create_report. Changed functionallity if create_report.py called directly

--- application/create_report.py
+++ application/create_report.py
@@ -1,3 +1,26 @@
+"""
+
+Application to create pdf report based on parameters passed into it
+
+@Params:
+        data_source         - Data source for report creation
+        report_name         - Title of the report and the file name
+        report_author       - Author of the report 
+        university_name     - Name of the university
+        university_school   - Name of the university school
+
+@Todo:
+    - Implement detect data source type to allow for differing file types/sources?
+    - Error handling
+    - Remove hardcoded elements
+    - Split functions further or into their own file
+    - Refactor the create_x_section functions into one function that takes a section arg 
+
+"""
+
+
+
+
 
 # Library Imports
 from fpdf import FPDF, TitleStyle, errors, TextMode
@@ -5,19 +28,28 @@ from fpdf.outline import build_outline, outline_as_str
 from fpdf.syntax import iobj_ref as pdf_ref
 import pandas
 import os
+import sys
 
 
 # Local Imports
 from amphibian import AmphibianData
 
-
-
-
-
-
 # Create report
 
-def create_report(data_source, report_name, report_author, university_name, university_school):
+def create_report(data_source: str, report_name: str, report_author: str, university_name: str, university_school: str) -> None:
+
+    """
+    Primary function that creates pdf object, sets fonts, calls component creation methods and saves output.
+
+    Args:
+        data_source         - Data source for report creation
+        report_name         - Title of the report and the file name
+        report_author       - Author of the report 
+        university_name     - Name of the university
+        university_school   - Name of the university school
+
+    """
+
 
     ds = read_data_source(data_source)
     pdf = FPDF()
@@ -29,8 +61,34 @@ def create_report(data_source, report_name, report_author, university_name, univ
     pdf.output(f"{'_'.join(report_name.split(' '))}.pdf")
 
 # create title page
-# #e30613
-def create_title_page(report_name, report_author, university_name, university_school, pdf) -> object:
+
+def create_title_page(report_name: str, report_author: str, university_name: str, university_school: str, pdf: object) -> object:
+
+    """
+    Creates a title page based upon passed args. Adds text and images to pdf.page() then returns pdf object.
+
+    Process:
+        Adds page
+        Designates contents section
+        Adds background image
+        Adds banner image
+        Adds Text
+        Returns pdf obj
+
+
+
+    Args:
+        data_source         - Data source for report creation
+        report_name         - Title of the report and the file name
+        report_author       - Author of the report 
+        university_name     - Name of the university
+        university_school   - Name of the university school
+
+
+    Return:
+        pdf(obj) - pdf object
+    """
+
     pdf = FPDF()
     pdf.add_page()
     pdf.start_section(name="Title Page", level=0)
@@ -69,8 +127,20 @@ def create_title_page(report_name, report_author, university_name, university_sc
 
     return pdf
 
+# Contents page
 
-def create_contents_page(pdf):
+def create_contents_page(pdf: object) -> object:
+
+    """
+    Creates space for contents to be created in the document. Currently hardcoded. Will throw exception if the space isn't the correct amount.
+
+    Args:
+        pdf - pdf object
+
+    Return:
+        pdf - pdf object
+    """
+
     pdf.add_page()
     pdf.set_text_color(0,0,0)
     pdf.start_section(name="Table Of Contents", level=0)
@@ -78,7 +148,26 @@ def create_contents_page(pdf):
     return pdf
 
 
-def create_report_order_sections(ds, pdf):
+
+def create_report_order_sections(ds: object, pdf: object) -> object:
+
+    """
+    Filter function that splits the data frame (ds) into smaller dataframes based on the 'Order' column in the dataframe.  
+
+    Process:
+        Creates dataframe subsections
+        Sorts the new dataframes.
+        Loops through each of smaller dataframes to create contents section and level
+        Writes section title 
+        Passes section to the next filter function.
+
+    Args:
+        ds - Pandas Dataframe object
+        pdf - pdf object
+
+    Returns:
+        pdf - pdf object
+    """
 
     order = ds["Order"]
     order_names = order.value_counts().index.tolist()
@@ -96,7 +185,26 @@ def create_report_order_sections(ds, pdf):
 
     return pdf
 
-def create_report_family_sections(section_list, pdf):
+def create_report_family_sections(section_list: object, pdf:object) -> object:
+
+    """
+    Filter function that splits the data frame (section_list) into smaller dataframes based on the 'Family' column in the dataframe.  
+
+    Process:
+        Creates dataframe subsections
+        Sorts the new dataframes.
+        Loops through each of smaller dataframes to create contents section and level
+        Writes section title 
+        Passes section to the next filter function.
+
+    Args:
+        section_list - Pandas Dataframe object
+        pdf - pdf object
+
+    Returns:
+        pdf - pdf object
+    """
+
     family = section_list["Family"]
     family_names = family.value_counts().index.tolist()
     family_names.sort()
@@ -111,7 +219,26 @@ def create_report_family_sections(section_list, pdf):
 
     return pdf
 
-def create_report_genus_sections(section_list, pdf):
+def create_report_genus_sections(section_list: object, pdf: object) -> object:
+
+    """
+    Filter function that splits the data frame (ds) into smaller dataframes based on the 'Family' column in the dataframe.  
+
+    Process:
+        Creates dataframe subsections
+        Sorts the new dataframes.
+        Loops through each of smaller dataframes to create contents section and level
+        Writes section title 
+        Passes section to the create report pages function.
+
+    Args:
+        section_list - Pandas Dataframe object
+        pdf - pdf object
+
+    Returns:
+        pdf - pdf object
+    """
+
     genus = section_list["Genus"]
     genus_names = genus.value_counts().index.tolist()
     genus_names.sort()
@@ -128,16 +255,45 @@ def create_report_genus_sections(section_list, pdf):
 
 # Create pages
 def create_report_section_pages(section, pdf):
+
+    """
+    Takes the section passed to it. Passes section to 'create_amphibian_list' function to get the data transformed into a list of amphibian objects. Sorts the list by name. Loops through list of amphibian objects and passes them to the 'create_report_section_page' function.
+
+    Args:
+        section - pandas dataframe object
+        pdf - pdf object
+
+    Return: 
+        pdf - pdf object
+
+    """
+
+
     amp_list = create_amphibian_list(section)
     amp_list = sorted(amp_list, key=lambda a: a.species)
+
+    # Hardcoded example image will have to replace this 
     amp_list[0].image_url_male = "images/f1.jpg"
     amp_list[0].image_url_female = "images/f2.jpg"
+
     for amp in amp_list:
         pdf = create_report_section_page(amp, pdf)
     return pdf
 
 # create page
 def create_report_section_page(amp, pdf):
+
+    """
+    Takes an instance of Amphibian and creates a page. Adds title. Adds images (insert_species_images). Adds data table (create_report_page_table). Then returns pdf object. 
+
+    Args:
+        amp - Amphibian object
+        pdf - pdf object
+
+    Return: 
+        pdf - pdf object
+    """
+
     pdf.start_section(name=amp.get_short_name(), level=3)
     pdf.set_font('OpenSansBold', "", 24)
     pdf.ln(20)
@@ -151,6 +307,20 @@ def create_report_section_page(amp, pdf):
 # add images
 
 def insert_species_images(amp, pdf):
+
+    """
+    Takes an instance of Amphibian and pdf. Adds images (insert_species_images) to current pdf page. Then returns pdf object. 
+
+    FUNCTION CURRENTLY HARDCODED IMAGE VALUES
+
+    Args:
+        amp - Amphibian object
+        pdf - pdf object
+
+    Return: 
+        pdf - pdf object
+    """
+
     WIDTH = 210
     pdf.ln(20)
     pdf.set_font("OpenSansBold", "", 16)
@@ -181,6 +351,19 @@ def insert_species_images(amp, pdf):
 
 # create data table
 def create_report_page_table(amphibian_data, pdf):
+
+    """
+    Takes an instance of Amphibian and pdf. Adds the data in object to current pdf page in tabular format. Then returns pdf object. 
+
+    Args:
+        amp - Amphibian object
+        pdf - pdf object
+
+    Return: 
+        pdf - pdf object
+    """
+
+
     pdf.ln(20)
     pdf.set_font("OpenSansBold", "", 16)
     pdf.write(5, "Species Data:")
@@ -209,6 +392,12 @@ def create_report_index():
 def read_data_source(file_name: str) -> object:
     """
     Read the data_source file and transform it into a pandas dataframe
+
+    Args:
+        file_name - String path to file
+
+    Return
+        df - Pandas dataframe object
     """
 
     df = pandas.read_excel(file_name)
@@ -220,6 +409,12 @@ def create_amphibian_list(data_section: object) -> list:
     
     """
     Takes a row of data from the dataframe and extracts values and instantiates an instance of the AmphibianData class
+
+    Args:
+        data_section - Pandas dataframe object
+
+    Return:
+        amphibian_list - list of amphibian objects
     """
 
     amphibian_list = []
@@ -239,6 +434,15 @@ def create_amphibian_list(data_section: object) -> list:
 
 
 def render_toc(pdf, outline):
+
+    """
+    Function to render table of contents - taken from example code of FPDF2 documentation
+
+    Args:
+        pdf - pdf object
+        outline - outline object
+    """
+
     pdf.ln(20)
     pdf.set_font("Helvetica", size=24)
     pdf.underline = False
@@ -270,12 +474,16 @@ def render_toc(pdf, outline):
 
 if __name__ == "__main__":
 
-    data_source = "AMPH PILOT DATASET_V5_25.June.2022.xlsx"
-    report_name = "Test Report".upper()
-    report_author = "Daniel Pincheira-Donoso".upper()
-    university_name = "Queen's University Belfast".upper()
-    university_school = "School Of Biological Sciences".upper()
+    args = sys.argv
 
 
+    data_source = args[1]
+    report_name = args[2].upper()
+    report_author = args[3].upper()
+    university_name = args[4].upper()
+    university_school = args[5].upper()
 
+
+    print(f"Creating Report: {report_name}.pdf")
     create_report(data_source, report_name, report_author, university_name, university_school)
+    print(f"Report Complete: {report_name}.pdf")
