c0e6503c9b12e9007f81a5eab17c75a533058a33
Ciaran Cushnahan <ccushnahan01@qub.ac.uk>
2022-08-11 16:47:01 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+Python setup file to organise package setup
+"""
+
+
+import os
+
+from setuptools import find_packages, setup
+
+setup(
+    name="report-generator",
+    version="0.0.1",
+    description="Generate reports from amphibian info dataset",
+    long_description=open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
+    ).read(),
+    long_description_content_type="text/markdown",
+    author="Ciaran Cushnahan",
+    packages=find_packages(exclude=["report_generator.tests"]),
+    install_requires=[
+        "setuptools",
+        "docopt",
+        "pyQt5",
+        "pandas",
+        "openpyxl",
+        "black",
+        "loguru",
+        "fpdf2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "report-generator = report_generator.app:main",
+        ]
+    },
+)
